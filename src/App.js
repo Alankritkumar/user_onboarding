8f461af3218a35cb0599b3e95ac11e8f014eb174
Alankrit <kumar02.alankrit@gmail.com>
2022-08-13 11:41:31 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -13,30 +13,6 @@ import pic3 from './Img/congratulaations.jpg';
 function App() {
   let headerTitle = "Eden";
   const [step =1, setStep] = useState();
-
-  const [FormHeaderLines, setFormHeaderLines] = useState([
-    {
-      sno: 1,
-      title: "Welcome!First things first...",
-      desc: "You can always change them later."
-    },
-    {
-      sno: 2,
-      title: "Let's setup a home for all your work",
-      desc: "You can always create amnother workspace late."
-    },
-    {
-      sno: 3,
-      title: "How are you planning to use Eden?",
-      desc: "we'll streamline your setup experience accordingly."
-    },
-    {
-      sno: 4,
-      title: "Congratulations, Eren!",
-      desc: "You have completed onboarding, you can start using the Eden!"
-    }
-  ]);
-
   const StepIncrement = (step) => {
     if(step<4){
       setStep(step++)
@@ -49,7 +25,7 @@ function App() {
       <div>
         <Header title={headerTitle} logo={logo} />
         <ProgressTab ProgressTab={step} />
-        <FormHeader FormHeaderLines={FormHeaderLines[step - 1]} pic3={pic3} />
+        <FormHeader step={step} pic3={pic3} />
         <Form step={step} pic1={pic1} pic2={pic2}/>
         <Button StepIncrement={StepIncrement} step={step} />
       </div>
--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -1,9 +1,31 @@
-const FormHeader = ({ FormHeaderLines, pic3 }) => {
+const FormHeader = ({ step, pic3 }) => {
+  const FormHeaderLines =[
+    {
+      sno: 1,
+      title: "Welcome!First things first...",
+      desc: "You can always change them later."
+    },
+    {
+      sno: 2,
+      title: "Let's setup a home for all your work",
+      desc: "You can always create amnother workspace late."
+    },
+    {
+      sno: 3,
+      title: "How are you planning to use Eden?",
+      desc: "we'll streamline your setup experience accordingly."
+    },
+    {
+      sno: 4,
+      title: "Congratulations, Eren!",
+      desc: "You have completed onboarding, you can start using the Eden!"
+    }
+  ];
   return (
     <div className="FormHeaderFont">
-      {(FormHeaderLines.sno==4) ? <div><img src={pic3} style={{ textAlign: 'center' }} /></div> :""}
-      <h3>{FormHeaderLines.title}</h3>
-      <p>{FormHeaderLines.desc}</p>
+      {(FormHeaderLines[step-1].sno==4) ? <div><img src={pic3} style={{ textAlign: 'center' }} /></div> :""}
+      <h3>{FormHeaderLines[step-1].title}</h3>
+      <p>{FormHeaderLines[step-1].desc}</p>
     </div>
   )
 }
